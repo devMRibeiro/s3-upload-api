0861e3579e694e8f3b79ebe87a46076d223138a1
devMRibeiro <michael.douglas2020@hotmail.com>
2026-09-19 12:23:48 -0300

    First commit

---
+++ src/test/java/br/com/devmribeiro/s3_uploader/S3UploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devmribeiro.s3_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3UploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
