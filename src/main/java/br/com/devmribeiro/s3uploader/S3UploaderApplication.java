bbf628915188a604a5b9dfbf49a2612172bcfd5b
devMRibeiro <michael.douglas2020@hotmail.com>
2026-09-22 02:32:45 -0300

    chore: remove JPA dependency

--- src/main/java/br/com/devmribeiro/s3uploader/S3UploaderApplication.java
+++ src/main/java/br/com/devmribeiro/s3uploader/S3UploaderApplication.java
@@ -2,11 +2,8 @@ package br.com.devmribeiro.s3uploader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-	    exclude = DataSourceAutoConfiguration.class
-)
+@SpringBootApplication
 public class S3UploaderApplication {
 
 	public static void main(String[] args) {
